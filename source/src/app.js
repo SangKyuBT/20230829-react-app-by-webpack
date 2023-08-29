1effcdc9ef6d8e3ba8763efeb0fa97f5908fddd6
SangKyuBT <SangKyuBT@github.com>
2023-08-29 23:22:01 +0900

    2.3 리액트 주입

---
+++ source/src/app.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="app">React App 매뉴얼 구성</div>
+  )
+}
+
+export default App
--- source/src/index.js
+++ source/src/index.js
@@ -1,1 +1,16 @@
-console.log( 'Hello Word!' )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './app'
+
+//DOM 노드
+const root = ReactDOM.createRoot(
+  document.getElementById( 'root' )
+)
+
+//DOM 노드에 React 요소 렌더링
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+)
